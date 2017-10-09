b4b21e9943c3333c53516daac681f1d38048d260
Thales Araujo <thalesaraujord@gmail.com>
2017-10-08 21:20:58 -0300

    JavaFX bosta

--- src/chat/FXMLDocumentController.java
+++ src/chat/FXMLDocumentController.java
@@ -10,7 +10,9 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 /**
  *
@@ -19,12 +21,15 @@ import javafx.scene.control.Label;
 public class FXMLDocumentController implements Initializable {
     
     @FXML
-    private Label label;
+    private Button botaoEnviar;
+    private TextArea txtareaMensagem;
+    private TextArea txtareaConversa;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void enviarMesagem(ActionEvent event) {
+        
+        
+        
     }
     
     @Override
