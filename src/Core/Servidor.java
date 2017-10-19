1b7b208fb06a2f9239386ec65d8f9cd3dd9dfcf5
Gabriel Victor <gabrielvgx@gmail.com>
2017-10-19 14:39:20 -0200

    UPDATE de Gerenciador de Clientes

--- src/Core/Servidor.java
+++ src/Core/Servidor.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
+// Servidor
 public class Servidor {
     public static void main(String[] args) {
         while(true){
